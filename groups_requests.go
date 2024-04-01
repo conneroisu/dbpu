d4800a911d5fcbd7632edf14dde16407e4a0d7ea
connero <88785126+conneroisu@users.noreply.github.com>
2024-04-01 14:34:43 -0500

    fix groups_requests client to use the correct endpoint from the clinet

--- groups_requests.go
+++ groups_requests.go
@@ -8,7 +8,7 @@ import (
 
 // newRemoveLocationFromGroupReq creates a request for removing a location from a group.
 func (c *Client) newRemoveLocationFromGroupReq(orgName, apiToken, groupName, location string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s/locations/%s", orgName, groupName, location)
+	url := fmt.Sprintf("/organizations/%s/groups/%s/locations/%s", c.BaseURL, orgName, groupName, location)
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error reading request. %v", err)
@@ -19,7 +19,7 @@ func (c *Client) newRemoveLocationFromGroupReq(orgName, apiToken, groupName, loc
 
 // newInvalidateGroupTokensRequest creates a request for invalidating all tokens for a group.
 func (c *Client) newInvalidateGroupTokensReq(orgName, apiToken, groupName string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s/auth/rotate", orgName, groupName)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s/auth/rotate", c.BaseURL, orgName, groupName)
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("Error reading request. %v", err)
@@ -30,7 +30,7 @@ func (c *Client) newInvalidateGroupTokensReq(orgName, apiToken, groupName string
 
 // newCreateGroupReq creates a request for creating a group in an organization.
 func (c *Client) newCreateGroupReq(orgName, apiToken, groupName, location string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups", orgName)
+	url := fmt.Sprintf("%s/organizations/%s/groups", c.BaseURL, orgName)
 	payload := fmt.Sprintf(`{"name": "%s", "location": "%s"}`, groupName, location)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
 	if err != nil {
@@ -43,7 +43,7 @@ func (c *Client) newCreateGroupReq(orgName, apiToken, groupName, location string
 
 // newGroupTokenReq creates a request for creating a token for a group.
 func (c *Client) newGroupTokenReq(orgName, apiToken, groupName, expiration, authorization string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s/auth/tokens?expiration=%s&authorization=%s", orgName, groupName, expiration, authorization)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s/auth/tokens?expiration=%s&authorization=%s", c.BaseURL, orgName, groupName, expiration, authorization)
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request. %v", err)
@@ -55,7 +55,7 @@ func (c *Client) newGroupTokenReq(orgName, apiToken, groupName, expiration, auth
 
 // newUpdateGroupReq creates a request for updating a group.
 func (c *Client) newUpdateGroupReq(orgName, apiToken, groupName string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s/update", orgName, groupName)
+	url := fmt.Sprintf(c.BaseURL+"/organizations/%s/groups/%s/update", orgName, groupName)
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
@@ -66,9 +66,7 @@ func (c *Client) newUpdateGroupReq(orgName, apiToken, groupName string) (*http.R
 
 // newGetGroupReq creates a request for getting a group.
 func (c *Client) newGetGroupReq(orgName, apiToken, groupName string) (*http.Request, error) {
-	url := fmt.Sprintf("%s/organizations/%s/groups/%s",
-		tursoEndpoint, orgName, groupName,
-	)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s", c.BaseURL, orgName, groupName)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
@@ -80,7 +78,7 @@ func (c *Client) newGetGroupReq(orgName, apiToken, groupName string) (*http.Requ
 
 // newAddLocationReq creates a request for adding a location to a group.
 func (c *Client) newAddLocationReq(orgName, apiToken, groupName, location string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s/locations/%s", orgName, groupName, location)
+	url := fmt.Sprintf("/organizations/%s/groups/%s/locations/%s", c.BaseURL, orgName, groupName, location)
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
@@ -91,7 +89,7 @@ func (c *Client) newAddLocationReq(orgName, apiToken, groupName, location string
 
 // newListGroupsReq creates a request for listing groups.
 func (c *Client) newListGroupsReq(orgName, apiToken string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups", orgName)
+	url := fmt.Sprintf("/organizations/%s/groups", c.BaseURL, orgName)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
@@ -102,7 +100,7 @@ func (c *Client) newListGroupsReq(orgName, apiToken string) (*http.Request, erro
 
 // newDeleteGroupReq creates a request for deleting a group.
 func (c *Client) newDeleteGroupReq(orgName, apiToken, groupName string) (*http.Request, error) {
-	url := fmt.Sprintf(tursoEndpoint+"/organizations/%s/groups/%s", orgName, groupName)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s", c.BaseURL, orgName, groupName)
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
@@ -113,7 +111,7 @@ func (c *Client) newDeleteGroupReq(orgName, apiToken, groupName string) (*http.R
 
 // newTransferGroupReq creates a request for transferring a group to a new organization.
 func (c *Client) newTransferGroupReq(orgName, apiToken, groupName, newOrgName string) (*http.Request, error) {
-	url := fmt.Sprintf("%s/organizations/%s/groups/%s/transfer", tursoEndpoint, orgName, groupName)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s/transfer", c.BaseURL, orgName, groupName)
 	payload := fmt.Sprintf(`{"organization": "%s"}`, newOrgName)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
 	if err != nil {
@@ -126,7 +124,7 @@ func (c *Client) newTransferGroupReq(orgName, apiToken, groupName, newOrgName st
 
 // newAddLocationToGroupReq creates a request for adding a location to a group.
 func (c *Client) newAddLocationToGroupReq(orgName, apiToken, groupName, location string) (*http.Request, error) {
-	url := fmt.Sprintf("%s/organizations/%s/groups/%s/locations/%s", tursoEndpoint, orgName, groupName, location)
+	url := fmt.Sprintf("%s/organizations/%s/groups/%s/locations/%s", c.BaseURL, orgName, groupName, location)
 	req, err := http.NewRequest("POST", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error reading request. %v", err)
